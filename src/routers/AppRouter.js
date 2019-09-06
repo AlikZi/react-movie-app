8855705d10b378ac6603316cf21076ae3027f829
Alex Zonis <zonis7@gmail.com>
2019-09-05 19:32:06 -0500

    Add Header Component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import {createBrowserHistory} from 'history';
+import Header from '../components/Header';
 import MainPage from '../components/MainPage';
 import MovieOverviewPage from '../components/MovieOverviewPage';
 import NotFoundPage from "../components/NotFoundPage";
@@ -10,6 +11,7 @@ export const history = createBrowserHistory();
 const AppRouter = () => (
     <Router history={history}>
         <div>
+            <Header/>
             <Switch>
                 <Route path="/" exact component={MainPage} />
                 <Route path="/movie/:movieId" component={MovieOverviewPage} />
