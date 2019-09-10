4d0542cf208fc2189ba1c782f446edb37fa78681
Alex Zonis <zonis7@gmail.com>
2019-09-10 18:59:32 -0500

    Complete design of Movielist and Moviecard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,15 +6,21 @@ import { URL_IMAGE } from '../api_source';
 const MovieCard = ({ movie }) => {
     const {id, title, release_date, vote_average, overview, poster_path } = movie;
     const date = moment(release_date, 'YYYY-MM-DD').format('MMMM D, YYYY');
+    const rate = vote_average.toFixed(1);
     return (
-    <div className='movie-card'>
-        <img className='movie-card-poster' alt={title} src={`${URL_IMAGE}${poster_path}`}/>
-        <div className='movie-card-info'>
-            <h2 className='movie-card-info-title'>{title}</h2>
-            <p className='movie-card-info-date'>{date}</p>
-            <p className='movie-card-info-vote'>{vote_average}</p>
-            <p className='movie-card-info-overview'>{overview}</p>
-            <Link to={`/movie/${id}`}><p>More Info</p></Link>
+    <div className='moviecard'>
+    <Link to={`/movie/${id}`}><img className='moviecard-poster' alt={title} src={`${URL_IMAGE}${poster_path}`}/></Link>
+        <div className='moviecard-info'>
+            <div className='moviecard-info-header'>
+                <p className='moviecard-info-header-rate'>{rate}</p>
+                <div className='moviecard-info-header-text'>
+                    <h3 className='moviecard-info-header-text-title'>{title}</h3>
+                    <p className='moviecard-info-header-text-date'>{date}</p>
+                </div>
+            </div>
+            <p className='moviecard-info-overview'>{overview}</p>
+            
+            <Link style={{ textDecoration: 'none'}} to={`/movie/${id}`}><p className='moviecard-info-link'>More Info</p></Link>
         </div>
     </div>
 )};
