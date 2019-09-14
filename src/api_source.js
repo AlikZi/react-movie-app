42b570fc06873f73a1e0211e1aab53252f42c751
Alex Zonis <zonis7@gmail.com>
2019-09-14 13:00:09 -0500

    Style MovieOverviewPage, add responsiveness

--- src/api_source.js
+++ src/api_source.js
@@ -1,4 +1,4 @@
 export const URL_MOVIES='http://api.themoviedb.org/3/movie/popular?api_key=';
 export const API_KEY='d1d0ff2b8b31a90e89109439b6b4f2ad';
-export const URL_IMAGE='https://image.tmdb.org/t/p/w500/';
-export const URL_BACKDROP='https://image.tmdb.org/t/p/w1280/';
+export const URL_IMAGE='https://image.tmdb.org/t/p/w500';
+export const URL_BACKDROP='https://image.tmdb.org/t/p/w1280';
--- src/components/MovieOverviewPage.js
+++ src/components/MovieOverviewPage.js
@@ -7,11 +7,15 @@ export default class MovieOverviewPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = { movie: {},
-                   trailerKey: '' }
+                   videos: [] }
   }
   componentDidMount() {
+    // When compnent mounts scroll to the top of the page
+    window.scrollTo(0, 0);
     const id = this.props.match.params.movieId;
-    fetch(`http://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
+
+    // Fetch details about the movie from TMDB and set the state
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
       .then( res => res.json())
       .then( (data) => {
         console.log(data);
@@ -19,18 +23,17 @@ export default class MovieOverviewPage extends React.Component {
       .catch(err => 
       console.log(err));
 
-    fetch(`http://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`)
+    // Fetch videos(trailers) to the movie from TMDB and set the state
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`)
     .then(res => res.json())
     .then( data => {
-      const trailers = data.results.filter(video => video.type =="Trailer");
-      this.setState({ trailerKey: trailers[0].key})
+      console.log(data.results);
+      //const trailers = data.results.filter(video => video.type =="Trailer");
+      this.setState({ videos: data.results.slice(0, 2)})
     })
   }
   render() {
-    const opts = {
-      height: '390',
-      width: '640'
-    };
+    // Destructure movie for 
     const { 
       title,
       overview,
@@ -40,7 +43,24 @@ export default class MovieOverviewPage extends React.Component {
       vote_average,
       genres
           }= this.state.movie;
+    let rate;
+    if (vote_average){
+      rate = vote_average.toFixed(1);
+    }
+
     const date = moment(release_date, 'YYYY-MM-DD').format('MMMM D, YYYY');
+    // Create
+    const videos = this.state.videos.map(video =>{
+      return (
+      <div key={video.key} className="movie-videos__item">
+        <div className="video-container">
+            <iframe key={video.key} width="320" height="180" allowFullScreen={true} src={`https://www.youtube.com/embed/${video.key}`} frameBorder="0"/>
+        </div>
+        <p className="movie-videos__item--name">{video.name}</p>
+      </div>
+      );
+    });
+
     let genres_names;
     if(genres){
       genres_names = genres.map(genre => genre.name).join(', ')
@@ -57,23 +77,20 @@ export default class MovieOverviewPage extends React.Component {
               <div className="poster-title-box__info">
                 <h2 className="poster-title-box__info--title">{title}</h2>
                 <p className='poster-title-box__info--date'>{date}</p>
-                <p>Vote average: {vote_average}</p>
-                
-                <p>Genres: {genres_names}</p>
+                <p className='poster-title-box__info--genres'>Genres: {genres_names}</p>
               </div>
+              <p className="poster-title-box__rate">{rate}</p>
             </div>
-            <div className="overview-trailer-box">
+            {/*<div className="overview-trailer-box">*/}
               <div className="overview-text">
                 <h3>Overview</h3>
                 <p>{overview}</p>
               </div>
-              
-              <YouTube
-                videoId={this.state.trailerKey}
-                className="trailer"
-                opts={opts}
-              />
-            </div>
+              <h3 className="videos-header">Trailers</h3>
+              <div className="movie-videos">
+                {videos}
+              </div>
+          {/*</div>*/}
           </div>
         </div>
       </div>
